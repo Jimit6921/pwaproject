6c7ffebbeccc5dc1c7365b399157a42ed454191b
Jimit <140408993+Jimit6921@users.noreply.github.com>
2025-12-21 15:48:11 +0530

    Remove HealthController class and its methods

--- prodemo1/src/main/java/com/example/prodemo1/controller/HealthController.java
+++ prodemo1/src/main/java/com/example/prodemo1/controller/HealthController.java
@@ -1,14 +1,1 @@
-package com.example.prodemo1.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HealthController {
-
-    @GetMapping("/")
-    public String health() {
-        return "Application is running";
-    }
-}
 
