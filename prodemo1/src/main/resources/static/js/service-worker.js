b497709f6d3099623aa302f182c48b6b012b325c
Jimit <140408993+Jimit6921@users.noreply.github.com>
2025-12-24 19:50:07 +0530

    Update service-worker.js

--- prodemo1/src/main/resources/static/js/service-worker.js
+++ prodemo1/src/main/resources/static/js/service-worker.js
@@ -1,11 +1,18 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+const CACHE = "offline-register-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "register.html",
+        "main.js"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated.');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
